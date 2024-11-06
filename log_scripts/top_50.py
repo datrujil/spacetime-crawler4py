09d3be17acd1caa85acc218d95bfaa8ce688b2c3
Derek <datrujil@uci.edu>
2024-11-06 00:26:53 +0000

    Updated tokenizer

--- log_scripts/top_50.py
+++ log_scripts/top_50.py
@@ -9,7 +9,12 @@ STOP_WORDS = {
     "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll",
     "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most",
     "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours",
-    "also", "this", "will", "the"
+    "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so",
+    "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", 
+    "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until",
+    "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when",
+    "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would",
+    "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"
 }
 
 def top_50_tokens(file_list):
@@ -35,5 +40,5 @@ def top_50_tokens(file_list):
     for token, count in top_50:
         print(f"{token} -> {count}")
 
-# Example of how to use it
-# top_50_tokens(['crawler_log_1.txt', 'crawler_log_2.txt'])
+# Example Usage
+# top_50_tokens(['./crawled_logs/crawled_content_1.txt', './crawled_logs/crawled_content_2.txt', './crawled_logs/crawled_content_3.txt', './crawled_logs/crawled_content_4.txt', './crawled_logs/crawled_content_5.txt', './crawled_logs/crawled_content_6.txt', './crawled_logs/crawled_content_7.txt', './crawled_logs/crawled_content_8.txt', './crawled_logs/crawled_content_9.txt', './crawled_logs/crawled_content_10.txt', './crawled_logs/crawled_content_11.txt', './crawled_logs/crawled_content_12.txt', './crawled_logs/crawled_content_13.txt', './crawled_logs/crawled_content_14.txt'])
